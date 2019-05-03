5e65fc3e0633b9e11b573894cc1041c581bdb37d
Sean Murphy <filmmakersean@fastmail.net>
2019-05-03 18:36:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,4 @@
 def reverse_each_word(phrase)
   words = phrase.split
-  backwards = []
-  words.collect do |word|
-    backwards << word.reverse
-  end
-  backwards.join(" ")
+  (words.collect {|word| word.reverse}).join(" ")
 end
